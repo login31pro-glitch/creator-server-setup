c9f04b035acfa388ca9d05987a8050d9e3a0634b
login31pro-glitch <login31pro@gmail.com>
2026-06-24 08:10:03 -0600

    Delete add_delete_policies.sql
    I just realized that i ended up never using tournaments

--- add_delete_policies.sql
+++
@@ -1,7 +0,0 @@
-DROP POLICY IF EXISTS "Creators can delete their tournaments" ON public.tournaments;
-CREATE POLICY "Creators can delete their tournaments" ON public.tournaments FOR DELETE TO authenticated USING (auth.uid() = creator_id);
-
-DROP POLICY IF EXISTS "Creators can delete participants" ON public.tournament_participants;
-CREATE POLICY "Creators can delete participants" ON public.tournament_participants FOR DELETE TO authenticated USING (
-    EXISTS (SELECT 1 FROM public.tournaments WHERE id = tournament_id AND creator_id = auth.uid())
-);
